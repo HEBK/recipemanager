eeaa130e05f736e5d5eb63ab5f0331561ea764cc
Cr4zyFl1x <florian@kleine-vorholt.net>
2022-02-11 11:15:40 +0100

    logger

--- src/de/hebkstudents/recipemanager/RecipeManager.java
+++ src/de/hebkstudents/recipemanager/RecipeManager.java
@@ -1,8 +1,20 @@
 package de.hebkstudents.recipemanager;
 
+import eu.cr4zyfl1x.logger.LogType;
+import eu.cr4zyfl1x.logger.Logger;
+
+import java.util.Date;
+
 public class RecipeManager {
 
     public static void main(String[] args) {
 	    System.out.println("Hello World!");
+        initLogger();
+        Logger.log(LogType.INFORMATION, "Hruren");
+    }
+
+    private static void initLogger() {
+        Logger logger = new Logger("RecipeManager", new Date(), "resources/logs");
+        logger.load();
     }
 }

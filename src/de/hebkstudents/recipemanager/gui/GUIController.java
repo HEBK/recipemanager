731f15f08ab3a3e56a4c58cf5898f68316203bd5
Cr4zyFl1x <florian@kleine-vorholt.net>
2022-02-13 17:22:38 +0100

    Added: Basic GUI controlling

---
+++ src/de/hebkstudents/recipemanager/gui/GUIController.java
@@ -0,0 +1,86 @@
+package de.hebkstudents.recipemanager.gui;
+
+import de.hebkstudents.recipemanager.RecipeManager;
+import de.hebkstudents.recipemanager.gui.frames.Menu;
+import de.hebkstudents.recipemanager.gui.frametype.AppFrame;
+import eu.cr4zyfl1x.logger.LogType;
+import eu.cr4zyfl1x.logger.Logger;
+
+import javax.swing.*;
+
+public class GUIController {
+
+    private RecipeManager app;
+    private boolean running = false;
+
+    private AppFrame menu;
+
+    public GUIController(RecipeManager app)
+    {
+        init(app, false);
+    }
+
+    public GUIController(RecipeManager app, boolean run)
+    {
+        init(app, run);
+    }
+
+    /**
+     * Initializes the GUIController
+     * @param app App object
+     * @param run Run GUIController after initialization?
+     */
+    private void init(RecipeManager app, boolean run)
+    {
+        if (app == null) {
+            if (Logger.isLoaded()) {
+                Logger.log(LogType.CRITICAL, "GUIController could not be initialized! (Invalid App instance)");
+            }
+            System.exit(-1);
+        }
+
+        if (run) run();
+    }
+
+    /**
+     * Gets the App object for this GUIController
+     * @return App object
+     */
+    public RecipeManager getApp()
+    {
+        return app;
+    }
+
+    /**
+     * Runs the GUIController if it is not already running
+     */
+    public void run() {
+        if (running) {
+            Logger.log(LogType.WARNING, "GUIController is already running!");
+            return;
+        }
+        running = true;
+        Logger.log(LogType.INFORMATION, "Running GUI Controller...");
+        menu = new Menu(this);
+    }
+
+    /**
+     * Tries to focus a frame by its object
+     * @param frame Object of the frame that should be focussed
+     * @return true if frame was focussed
+     */
+    private boolean focusFrame(JFrame frame)
+    {
+        if (frame != null && frame.isDisplayable()) {
+
+            // Set frame visible
+            frame.setVisible(true);
+
+            // Try focussing frame
+            frame.requestFocus();
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ src/de/hebkstudents/recipemanager/gui/frames/Menu.java
@@ -0,0 +1,36 @@
+package de.hebkstudents.recipemanager.gui.frames;
+
+import de.hebkstudents.recipemanager.gui.GUIController;
+import de.hebkstudents.recipemanager.gui.frametype.AppFrame;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import static de.hebkstudents.recipemanager.storage.StaticProperties.*;
+
+public class Menu extends AppFrame {
+    private JPanel root;
+    private JLabel appnameLabel;
+    private JButton addRecipeButton;
+    private JButton showRecipesButton;
+    private JButton filterRecipesButton;
+    private JButton ingredientsButton;
+    private JButton closeButton;
+    private JLabel versionLabel;
+
+    public Menu(GUIController controller)
+    {
+        super(controller, buildFrameTitle("Hauptmenü"), DEFAULT_DIMENSION, true);
+        init();
+    }
+
+    @Override
+    protected void init() {
+        add(root);
+        setDefaultCloseOperation(AppFrame.EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(750, 550));
+        versionLabel.setText("v" + VERSION);
+        appnameLabel.setText(APPNAME);
+    }
+}

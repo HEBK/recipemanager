18ce90fb802b84252415f2e49b125511a25339fa
Liz1312 <liz.rosenberger@hebk.de>
2022-02-16 10:28:45 +0100

    Rezepte hinzufügen anfang der GUI

---
+++ src/de/hebkstudents/recipemanager/gui/frames/recipe/AddRecipe.java
@@ -0,0 +1,22 @@
+package de.hebkstudents.recipemanager.gui.frames.recipe;
+
+import de.hebkstudents.recipemanager.gui.GUIController;
+import de.hebkstudents.recipemanager.gui.frametype.AppFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
+import de.hebkstudents.recipemanager.storage.StaticProperties.*;
+
+public class AddRecipe extends AppFrame {
+    private JPanel root;
+
+    public AddRecipe(GUIController controller) {
+        super(controller, buildFrameTitle("Rezept hinzufügen"), new Dimension(750, 550));
+    }
+
+    @Override
+    protected void init() {
+
+    }
+}

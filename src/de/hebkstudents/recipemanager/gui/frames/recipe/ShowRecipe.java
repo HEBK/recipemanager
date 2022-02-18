84fc66e81965a6ffad7e505c27fd625c6a783b97
Cr4zyFl1x <florian@kleine-vorholt.net>
2022-02-18 11:15:54 +0100

    Rezepte label hinzugefügt

---
+++ src/de/hebkstudents/recipemanager/gui/frames/recipe/ShowRecipe.java
@@ -0,0 +1,28 @@
+package de.hebkstudents.recipemanager.gui.frames.recipe;
+
+import de.hebkstudents.recipemanager.gui.GUIController;
+import de.hebkstudents.recipemanager.gui.frametype.AppFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ShowRecipe extends AppFrame {
+    private JPanel root;
+    private JTextArea recipeName;
+    private JTextArea description;
+    private JCheckBox vegetarischCheckBox;
+    private JCheckBox veganCheckBox;
+    private JButton umrechnenButton;
+    private JComboBox comboBox1;
+    private JTable ingredientsTable;
+
+    public ShowRecipe(GUIController controller) {
+        super(controller, buildFrameTitle("Rezept"), new Dimension(600, 400), true);
+        init();
+    }
+
+    @Override
+    protected void init() {
+
+    }
+}

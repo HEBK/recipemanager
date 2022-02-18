307716fc5d8dbee0afce0e6f774d0efc7f907ed9
Cr4zyFl1x <florian@kleine-vorholt.net>
2022-02-18 10:10:05 +0100

    Fixed: BRoken class attributes

--- src/de/hebkstudents/recipemanager/gui/frames/recipe/AddRecipe.java
+++ src/de/hebkstudents/recipemanager/gui/frames/recipe/AddRecipe.java
@@ -6,8 +6,6 @@ import de.hebkstudents.recipemanager.gui.frametype.AppFrame;
 import javax.swing.*;
 import java.awt.*;
 
-import de.hebkstudents.recipemanager.storage.StaticProperties.*;
-
 public class AddRecipe extends AppFrame {
     private JPanel root;
     private JTextPane rezeptNameTextPane;
@@ -15,10 +13,10 @@ public class AddRecipe extends AppFrame {
     private JTextPane schwierigkeitTextPane;
     private JTextPane zeitTextPane;
     private JTextPane zutatenTextPane;
-    private JTextPane rezeptHinzufügenTextPane;
+    private JTextPane addRecipeTextPane;
     private JTextArea textArea1;
     private JTable table1;
-    private JButton hinzufügen;
+    private JButton addRecipeButton;
 
     public AddRecipe(GUIController controller) {
         super(controller, buildFrameTitle("Rezept hinzufügen"), new Dimension(600, 400), true);

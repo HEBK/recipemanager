a5f1364283c5d3dc9799adf69dc9a26e1283af74
Cr4zyFl1x <florian@kleine-vorholt.net>
2022-03-02 10:07:28 +0100

    Zutaten Filter hinzugefügt

---
+++ src/de/hebkstudents/recipemanager/gui/frames/ingredient/IngredientFilter.java
@@ -0,0 +1,12 @@
+package de.hebkstudents.recipemanager.gui.frames.ingredient;
+
+import javax.swing.*;
+
+public class IngredientFilter {
+    private JPanel root;
+    private JButton suchenButton;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JCheckBox vegetarischCheckBox;
+    private JCheckBox veganCheckBox;
+}

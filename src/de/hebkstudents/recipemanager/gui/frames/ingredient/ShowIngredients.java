f6224a67be6b77e58df3195a9b052e3473acba04
Cr4zyFl1x <florian@kleine-vorholt.net>
2022-03-02 10:16:15 +0100

    ShowIngredients hinzugefügt

---
+++ src/de/hebkstudents/recipemanager/gui/frames/ingredient/ShowIngredients.java
@@ -0,0 +1,12 @@
+package de.hebkstudents.recipemanager.gui.frames.ingredient;
+
+import javax.swing.*;
+
+public class ShowIngredients {
+    private JPanel root;
+    private JButton filterButton;
+    private JButton zutatHinzufügenButton;
+    private JButton zutatLöschenButton;
+    private JButton zutatBearbeitenButton;
+    private JTable table1;
+}

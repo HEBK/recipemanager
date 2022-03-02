f102668cdc22683980af4a0a8cabe2176cdc75c3
Cr4zyFl1x <florian@kleine-vorholt.net>
2022-03-02 10:54:40 +0100

    Syntax RecipeFilter

--- src/de/hebkstudents/recipemanager/gui/frames/ingredient/IngredientFilter.java
+++ src/de/hebkstudents/recipemanager/gui/frames/ingredient/IngredientFilter.java
@@ -1,12 +1,28 @@
 package de.hebkstudents.recipemanager.gui.frames.ingredient;
 
-import javax.swing.*;
+import de.hebkstudents.recipemanager.gui.GUIController;
+import de.hebkstudents.recipemanager.gui.frametype.AppFrame;
 
-public class IngredientFilter {
+import javax.swing.*;
+import java.awt.*;
+
+import static de.hebkstudents.recipemanager.storage.StaticProperties.DEFAULT_DIMENSION;
+
+public class IngredientFilter extends AppFrame {
     private JPanel root;
     private JButton searchButton;
     private JTextField textField1;
     private JComboBox comboBox1;
     private JCheckBox vegetarianCheckBox;
     private JCheckBox veganCheckBox;
+
+    public IngredientFilter(GUIController controller) {
+        super(controller, buildFrameTitle("Zutatenfilter"), DEFAULT_DIMENSION, true);
+        init();
+    }
+
+    @Override
+    protected void init() {
+
+    }
 }
--- src/de/hebkstudents/recipemanager/gui/frames/recipe/RecipeFilter.java
+++ src/de/hebkstudents/recipemanager/gui/frames/recipe/RecipeFilter.java
@@ -1,8 +1,13 @@
 package de.hebkstudents.recipemanager.gui.frames.recipe;
 
+import de.hebkstudents.recipemanager.gui.GUIController;
+import de.hebkstudents.recipemanager.gui.frametype.AppFrame;
+
 import javax.swing.*;
 
-public class RecipeFilter {
+import static de.hebkstudents.recipemanager.storage.StaticProperties.DEFAULT_DIMENSION;
+
+public class RecipeFilter extends AppFrame {
     private JPanel root;
     private JButton searchButton;
     private JTextField textField1;
@@ -11,4 +16,13 @@ public class RecipeFilter {
     private JComboBox timeComboBox;
     private JCheckBox vegetarianCheckBox;
     private JCheckBox veganCheckBox;
+
+    public RecipeFilter(GUIController controller) {
+        super(controller, buildFrameTitle("Rezeptefilter"), DEFAULT_DIMENSION, true);
+        init();
+    }
+    @Override
+    protected void init() {
+
+    }
 }

271001dce7863a8bad5b0550a02e45dc2107bb2a
Cr4zyFl1x <florian@kleine-vorholt.net>
2022-03-02 10:45:06 +0100

    Syntax ShowIngredients

--- src/de/hebkstudents/recipemanager/gui/frames/ingredient/ShowIngredients.java
+++ src/de/hebkstudents/recipemanager/gui/frames/ingredient/ShowIngredients.java
@@ -1,12 +1,27 @@
 package de.hebkstudents.recipemanager.gui.frames.ingredient;
 
+import de.hebkstudents.recipemanager.gui.GUIController;
+import de.hebkstudents.recipemanager.gui.frametype.AppFrame;
+
 import javax.swing.*;
 
-public class ShowIngredients {
+import static de.hebkstudents.recipemanager.storage.StaticProperties.DEFAULT_DIMENSION;
+
+public class ShowIngredients extends AppFrame {
     private JPanel root;
     private JButton filterButton;
     private JButton addIngredientButton;
     private JButton deleteIngredientButton;
     private JButton updateIngredientButton;
     private JTable table1;
+
+    public ShowIngredients(GUIController controller) {
+        super(controller, buildFrameTitle("Zutatenliste"), DEFAULT_DIMENSION, true);
+        init();
+    }
+
+    @Override
+    protected void init() {
+        add(root);
+    }
 }

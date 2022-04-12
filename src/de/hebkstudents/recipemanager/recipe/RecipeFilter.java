e90a5e03469d696d88f7e4e7d34204af5da2f10f
Florian J. Kleine-Vorholt <florian@kleine-vorholt.net>
2022-04-12 13:59:51 +0200

    Updated: Recipe Filter functionality & JavaDoc Fixed: Typo in JavaDocs of IngredientFilter
    Signed-off-by: Cr4zyFl1x <florian@kleine-vorholt.net>

--- src/de/hebkstudents/recipemanager/recipe/RecipeFilter.java
+++ src/de/hebkstudents/recipemanager/recipe/RecipeFilter.java
@@ -6,20 +6,67 @@ import de.hebkstudents.recipemanager.interfaces.SimpleFilter;
 
 /**
  * RecipeFilter
- * Not yet in use
+ * Class to generate RecipeFilter objects from to filter Recipes
  */
 public class RecipeFilter implements SimpleFilter {
 
+    /**
+     * Query string to filter recipe names
+     * (null -> All recipe names)
+     */
     private final String query;
+
+    /**
+     * RecipeCategory object to filter only for Recipes of the same category
+     * (null -> All categories)
+     */
     private final RecipeCategory category;
+
+    /**
+     * Maximum time of recipe to filter for
+     * (null -> All times)
+     */
     private final Integer time;
+
+    /**
+     * Difficulty Integer - Same and below
+     * (null -> All difficulties)
+     */
     private final Integer difficulty;
+
+    /**
+     * Boolean to filter only for vegetarian recipes
+     * (null -> No matter if vegetarian or not)
+     */
     private final Boolean isVegetarian;
+
+    /**
+     * Boolean to filter only for vegan recipes
+     * (null -> No matter if vegan or not)
+     */
     private final Boolean isVegan;
+
+    /**
+     * Array of Ingredient Object that must be part of the recipe
+     * (null -> Ingredients are ignored)
+     */
     private final Ingredient[] ingredients;
 
-    private String sqlOptions;
+    /**
+     * SQL-Options String to directly filter using SQL-Statement
+     */
+    private String sqlOptions = null;
 
+    /**
+     * Constructs a new RecipeFilter
+     * @param query Query string to filter recipe names (null -> All recipe names)
+     * @param time Maximum time of recipe to filter for (null -> All times)
+     * @param difficulty Difficulty Integer - Same and below (null -> All difficulties)
+     * @param isVegetarian Boolean to filter only for vegetarian recipes (null -> No matter if vegetarian or not)
+     * @param isVegan Boolean to filter only for vegan recipes (null -> No matter if vegan or not)
+     * @param ingredients Array of Ingredient Object that must be part of the recipe (null -> Ingredients are ignored)
+     * @param category RecipeCategory object to filter only for Recipes of the same category (null -> All categories)
+     */
     public RecipeFilter(String query, Integer time, Integer difficulty, Boolean isVegetarian, Boolean isVegan, Ingredient[] ingredients, RecipeCategory category)
     {
         this.query          = query;
@@ -31,6 +78,10 @@ public class RecipeFilter implements SimpleFilter {
         this.ingredients    = ingredients;
     }
 
+    /**
+     * Constructs a new RecipeFilter
+     * @param sqlOptions SQL-Options String to directly filter using SQL-Statement (e.g. "WHERE ...")
+     */
     public RecipeFilter(String sqlOptions)
     {
         this.query          = null;
@@ -43,6 +94,12 @@ public class RecipeFilter implements SimpleFilter {
         this.sqlOptions = sqlOptions;
     }
 
+    /**
+     * Checks whether a recipe matches the filter or not
+     * @param recipe Valid Recipe-Object
+     * @return true if filter matches
+     * @throws InvalidRecipeException If Recipe-Object is invalid
+     */
     public boolean matchesFilter(Recipe recipe) throws InvalidRecipeException {
         if (recipe == null || recipe.getName() == null || recipe.getCategory() == null || recipe.getIngredients() == null) {
             throw new InvalidRecipeException("The given Recipe object is invalid. One or more values are missing or the object is corrupted!");
@@ -86,28 +143,19 @@ public class RecipeFilter implements SimpleFilter {
         return true;
     }
 
+    /**
+     * Gets the filter options as SQL "WHERE ..." String
+     * @return SQL Conditional String
+     */
     @Override
     public String getSQLOptions() {
 
-        if (sqlOptions != null) return sqlOptions;
-        if (query == null && category == null && time == null && difficulty == null && isVegetarian == null && isVegan == null && ingredients == null) return null;
-
-        String categoryVal = (category != null) ? String.valueOf(category.getCategoryID()) : "";
-
-        String[] stmts = {
-                "r.name LIKE '%" + query + "%'",
-                "r.category = " + categoryVal,
-                "r.time <= " + time,
-                "r.difficulty <= " + difficulty,
-
-        };
-
-        return null;
+        return sqlOptions;
     }
 
     @Override
     public String getSQLQuery() {
-        return null;
+        return "SELECT * FROM Recipe " + getSQLOptions();
     }
 
     public Boolean isVegan() {
